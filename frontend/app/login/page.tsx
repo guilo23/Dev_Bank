420b23d7b4bc936e909a34d427ab29ee9441d510
GuiDev <guilherme.alves1amo@gmail.com>
2025-02-11 19:25:50 -0300

    feat: loginPage

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginPageComponente from "./LoginPage";
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Welcome Dev Bank</h1>
+      <LoginPageComponente />
+    </div>
+  );
+}
+
+export default App;
