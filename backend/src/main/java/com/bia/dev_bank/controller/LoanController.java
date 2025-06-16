20e5271f1296ac536babeae8b74150190b5e6327
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2025-06-16 00:38:18 -0300

    fix: create loan endpoint

--- backend/src/main/java/com/bia/dev_bank/controller/LoanController.java
+++ backend/src/main/java/com/bia/dev_bank/controller/LoanController.java
@@ -1,26 +1,26 @@
 package com.bia.dev_bank.controller;
 
-import com.bia.dev_bank.dto.loanDTOs.LoanRequest;
-import com.bia.dev_bank.dto.loanDTOs.LoanResponse;
-import com.bia.dev_bank.service.LoanService;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bia.dev_bank.dto.loanDTOs.LoanRequest;
+import com.bia.dev_bank.dto.loanDTOs.LoanResponse;
+import com.bia.dev_bank.service.LoanService;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/bia/loans")
