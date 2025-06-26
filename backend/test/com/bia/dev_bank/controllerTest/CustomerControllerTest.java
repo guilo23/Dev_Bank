a525b1dcc5745ce2f0a0089160634d97db3bb183
Guilherme Alves <guilherme.alves1amo@gmail.com>
2025-06-26 20:58:07 -0300

    refact: some returns changed

--- backend/test/com/bia/dev_bank/controllerTest/CustomerControllerTest.java
+++ backend/test/com/bia/dev_bank/controllerTest/CustomerControllerTest.java
@@ -51,7 +51,7 @@ class CustomerControllerTest {
             new CustomerRequest(
                     "Maria", "maria@email.com", "123456", "1990-01-01", "123.456.789-00", "11999999999");
     CustomerResponse response =
-            new CustomerResponse("Maria", "maria@email.com", "123.456.789-00", "11999999999");
+            new CustomerResponse(1L,"Maria", "maria@email.com", "123.456.789-00", "11999999999");
 
     when(customerService.createCustomer(any())).thenReturn(response);
 
@@ -63,14 +63,15 @@ class CustomerControllerTest {
                             .with(csrf())
             )
             .andExpect(status().isOk())
-            .andExpect(content().string("congratulations Maria, you are a bia customer now"));
+            .andExpect(jsonPath("$.name").value("Maria"))
+            .andExpect(jsonPath("$.email").value("maria@email.com"));
   }
 
   @Test
   @WithMockUser
   void shouldGetCustomerById() throws Exception {
     CustomerResponse response =
-            new CustomerResponse("Maria", "maria@email.com", "123.456.789-00", "11999999999");
+            new CustomerResponse(1L,"Maria", "maria@email.com", "123.456.789-00", "11999999999");
 
     when(customerService.getCostumerById(1L)).thenReturn(response);
 
@@ -86,7 +87,7 @@ class CustomerControllerTest {
   void shouldUpdateCustomer() throws Exception {
     CustomerUpdate update = new CustomerUpdate("new@email.com", "newpass", "1188888888");
     CustomerResponse response =
-            new CustomerResponse("Maria", "new@email.com", "123.456.789-00", "1188888888");
+            new CustomerResponse(1L,"Maria", "new@email.com", "123.456.789-00", "1188888888");
 
     when(customerService.customerUpdate(Mockito.eq(1L), any())).thenReturn(response);
 
