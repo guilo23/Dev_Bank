ad0e536ebb5f1cc7f74a04c32912e6741298eca1
Guilherme Alves <guilherme.alves1amo@gmail.com>
2025-07-28 18:25:29 -0300

    feat: new payment endpoint

--- backend/src/main/java/com/bia/dev_bank/dto/card/CardResponse.java
+++ backend/src/main/java/com/bia/dev_bank/dto/card/CardResponse.java
@@ -5,13 +5,19 @@ import com.bia.dev_bank.entity.enums.*;
 import java.math.BigDecimal;
 
 public record CardResponse(
-    Long cardId, CardType cardType, String cardNumber, BigDecimal cardLimit, String customerName) {
+    Long cardId,
+    CardType cardType,
+    String cardNumber,
+    BigDecimal cardLimit,
+    BigDecimal cardBilling,
+    String customerName) {
   public CardResponse(Card card) {
     this(
         card.getId(),
         card.getCardType(),
         card.getCardNumber(),
         card.getCardLimit(),
+        card.getCardBilling(),
         card.getAccount().getCustomer().getName());
   }
 }
